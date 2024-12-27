a76ed79cad963d3cd6346dd564c45ba0f9b5ccf9
Musurmankul2004 <musumusulmankul@gmail.com>
2024-12-27 16:55:36 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Билдирүүңүз жөнөтүлдү! Биз жакын арада сиз менен байланышабыз.");
+});
